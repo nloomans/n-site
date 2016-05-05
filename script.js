7ec69b9001e5f2d8c7b98719ef54177f4dafcca6
Noah Loomans <cyborg@localhost.localdomain>
2016-05-05 15:51:21 +0200

    Init commit

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(_ => {
+      console.log('Server Worker Registered');
+      if (!navigator.serviceWorker.controller) {
+        document.querySelector('.snackbar').className
+          = 'snackbar snackbar-opened';
+        setTimeout(_ => {
+          document.querySelector('.snackbar').className
+            = 'snackbar';
+        }, 2000);
+      }
+    })
+    .catch(_ => {
+      console.log('Oops there was a problem');
+    });
+}
+
+window.addEventListener('DOMContentLoaded',event => {
+  console.log(event);
+});
---
+++ sw.js
@@ -0,0 +1,40 @@
+// timestamp to force update
+// hi
+
+var cacheName = 'n site v1';
+
+var thingsToCache = [
+  '/',
+  '/index.html',
+  '/style/main.css',
+  '/font-awesome/css/font-awesome.min.css',
+  '/script.js'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(thingsToCache).then(function() {
+        return self.skipWaiting();
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(cacheName).then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        var fetchPromise = fetch(event.request).then(function(networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        })
+        return response || fetchPromise;
+      })
+    })
+  );
+});
